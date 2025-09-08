bfd7f13f38022f9aa03a27770407d47edbc7c2d6
SHUBHAM CHAUHAN <shubhamchauhanpoly@gmail.com>
2025-09-08 11:20:48 +0530

    set up lazy ;oadding and sidebar and main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Suspense, lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Loader from "./components/Loader";
 
-const App = () => {
+// Lazy loading pages
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Resume = lazy(() => import("./pages/Resume"));
+const Portfolio = lazy(() => import("./pages/Portfolio"));
+const Services = lazy(() => import("./pages/Services"));
+const Contact = lazy(() => import("./pages/Contact"));
+
+const App =() => {
   return (
-    <div>App</div>
-  )
+    <div className="app-container">
+      {/* Fixed Sidebar */}
+      <Sidebar />
+
+      {/* Right Section */}
+      <div className="main-content">
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </div>
+  );
 }
 
 export default App;
