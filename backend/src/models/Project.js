17ecc1b33973b4acd7030bf2f4f7a94f813a15b9
SHUBHAM CHAUHAN <shubhamchauhanpoly@gmail.com>
2025-09-18 13:07:14 +0530

    in Project.js model add category for filtering

--- backend/src/models/Project.js
+++ backend/src/models/Project.js
@@ -5,6 +5,7 @@ const projectSchema = new Schema(
   {
     title: { type: String, required: true }, // Project name
     description: { type: String, required: true }, // Small summary
+    category: { type: String, enum: ["App", "Web"], required: true },
     image: { type: String }, // Image URL/path
     techStack: [String], // Array of techs
     github: { type: String }, // GitHub link
